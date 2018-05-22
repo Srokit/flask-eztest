c995bcb04deb9a1d22c5ddf9ee5d3e10b1c79df0
Stanley Rokita <srok35@gmail.com>
2018-05-22 16:23:18 -0400

    Add some simple print statements to handle excpetions with how the user setup enviroment before running eztest script

--- flaskeztest/__main__.py
+++ flaskeztest/__main__.py
@@ -7,10 +7,8 @@ import logging
 
 from helpers import parse_module_name_from_filepath
 
-USAGE_MESSAGE = "Usage: eztest"
 
-
-def flaskeztest_main(args=None):
+def flaskeztest_main():
     """
     Call this from main entry point of flaskeztest package.
     """
@@ -19,10 +17,21 @@ def flaskeztest_main(args=None):
     logging.getLogger('werkzeug').setLevel(logging.ERROR)
 
     flask_module = os.environ.get('FLASK_APP')
+    if flask_module is None:
+        print "ERROR: FLASK_APP enviromnet variable must be set"
+        sys.exit(1)
 
-    flask_module = import_module(parse_module_name_from_filepath(flask_module))
+    try:
+        flask_module = import_module(parse_module_name_from_filepath(flask_module))
+    except ImportError:
+        print "ERROR: Could not import flask module \"%s\"" % flask_module
+        sys.exit(1)
 
-    eztest = flask_module.eztest
+    try:
+        eztest = flask_module.eztest
+    except AttributeError:
+        print "ERROR: Could not find eztest object from attribute \"eztest\" on \"%s\" module" % flask_module
+        sys.exit(1)
 
     # Start up flask app and run our tests against it
     eztest.run()
