0079f1760134727e0b1f6905c28736ae3fd29d8a
Stanley Rokita <srok35@gmail.com>
2018-05-23 12:38:56 -0400

    Take out classifiers until I understand how to use them

--- setup.py
+++ setup.py
@@ -12,18 +12,6 @@ description = "Flask test generation based on db models."
 
 long_description = open('README.md').read()
 
-classifiers = [
-        'Development Status :: 1 - Development',
-
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-
-        'License :: OSI Approved :: MIT License',
-
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7'
-]
-
 setup(
     name='flaskeztest',
     version='0.1.0',
@@ -33,7 +21,6 @@ setup(
     long_description=long_description,
     install_requires=requirements,
     packages=['flaskeztest'],
-    classifiers=classifiers,
     entry_points={
         'console_scripts': [
             'eztest = flaskeztest.__main__:flaskeztest_main'
