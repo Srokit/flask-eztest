4a244e5b6e5e13b933101be18717d8469a570e5e
Stanley Rokita <srok35@gmail.com>
2018-05-30 09:53:24 -0400

    Add in some exceptions that can be figured out later

--- flaskeztest/exceptions.py
+++ flaskeztest/exceptions.py
@@ -4,3 +4,27 @@ class FixtureDoesNotExistError(BaseException):
 
     def __init__(self, fix_dir, fix_name):
         BaseException.__init__(self, self.MESSAGE_FORMAT % (fix_dir, fix_name))
+
+
+class FieldNotInFixtureError(BaseException):
+    MESSAGE_FORMAT = "Cannot find a field \"%s\".\"%s\" in fixture loaded \"%s\"."
+    MESSAGE_FORMAT2 = "Cannot find a field \"%s\".\"%s\" when a fixture was not loaded."
+
+    def __init__(self, model, field, fixture=None):
+        if fixture is None:
+            message = self.MESSAGE_FORMAT2 % (model, field)
+        else:
+            message = self.MESSAGE_FORMAT % (model, field, fixture)
+        BaseException.__init__(self, message)
+
+
+class ModelNotInFixtureError(BaseException):
+    MESSAGE_FORMAT = "Cannot find a model \"%s\" in fixture loaded \"%s\"."
+    MESSAGE_FORMAT2 = "Cannot find a model \"%s\" when a fixture was not loaded."
+
+    def __init__(self, model, fixture=None):
+        if fixture is None:
+            message = self.MESSAGE_FORMAT2 % model
+        else:
+            message = self.MESSAGE_FORMAT % (model, fixture)
+        BaseException.__init__(self, message)
