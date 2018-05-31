972d758eef6ca0d8d702fc0ac246127e16bf41f4
Stanley Rokita <srok35@gmail.com>
2018-05-31 13:44:22 -0400

    Allow importing EZTestSuite from flaskeztest package as well

--- flaskeztest/__init__.py
+++ flaskeztest/__init__.py
@@ -5,6 +5,7 @@ import capybara
 # Passing imports along so user's can import commonly used objects from root package
 from eztest import EZTest
 from eztestcase import EZTestCase
+from eztestsuite import EZTestSuite
 
 
 @capybara.register_driver("selenium")
